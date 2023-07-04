9192d9760be57f3e454f04a381afbe6d3114010d
Feermeringer <Tu correo>
2023-07-04 15:02:51 -0300

    cambios en el form

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -20,19 +20,25 @@ export default function Contacto() {
       message: message,
       subject: subject,
     };
-
+    const url = 'https://pampa-alta-back.onrender.com/contacts'
     // Enviar la solicitud al servidor
-    axios.post('https://pampa-alta-back.onrender.com/contacts', formData)
+    axios.post(url, formData)
       .then((response) => {
         // Mostrar una alerta de éxito
-        Swal('¡Éxito!', 'El formulario se ha enviado correctamente.', 'success');
+        Swal.fire('¡Éxito!', 'El formulario se ha enviado correctamente.', 'success');
+
+        // Resetear los campos del formulario
+        setName('');
+        setEmail('');
+        setMessage('');
+        setSubject('');
 
         // Aquí puedes agregar lógica adicional después de enviar con éxito el formulario
         console.log(response.data);
       })
       .catch((error) => {
         // Mostrar una alerta de error
-        Swal('¡Error!', 'Hubo un problema al enviar el formulario.', 'error');
+        Swal.fire('¡Error!', 'Hubo un problema al enviar el formulario.', 'error');
 
         // Aquí puedes agregar lógica adicional para manejar errores de la solicitud
         console.error(error);
@@ -41,58 +47,55 @@ export default function Contacto() {
 
   return (
     <div className='form'>
-    <form className="formulario" onSubmit={handleSubmit}>
-      <div className="contact-container">
-        <label htmlFor="name">Nombre:</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+      <form className="formulario" onSubmit={handleSubmit}>
+        <div className="contact-container">
+          <label htmlFor="name">Nombre:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="subject">Asunto:</label>
+          <select
+            id="subject"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+          >
+            <option value="">Seleccionar</option>
+            <option value="option1">Instalacion/Servicio</option>
+            <option value="option2">Paneles Solares</option>
+            <option value="option3">Inversores</option>
+            <option value="option4">Baterias De Litio</option>
+            <option value="option5">Termotanques</option>
+            <option value="option6">Climatizadores</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="message">Mensaje:</label>
+          <textarea
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
+      <div className="info-container">
+        <h2>Contactanos</h2>
+        <p>Mejora la eficiencia energética de tu hogar y reduce tu factura de la luz</p>
       </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="subject">Asunto:</label>
-        <select
-          id="subject"
-          value={subject}
-          onChange={(e) => setSubject(e.target.value)}
-        >
-          <option value="">Seleccionar</option>
-          <option value="option1">Instalacion/Servicio</option>
-          <option value="option2">Paneles Solares</option>
-          <option value="option3">Inversores</option>
-          <option value="option4">Baterias De Litio</option>
-          <option value="option5">Termotanques</option>
-          <option value="option6">Climatizadores</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="message">Mensaje:</label>
-        <textarea
-          id="message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-      </div>
-      <button type="submit">Enviar</button>
-    </form>
-    <div className="info-container">
-     <h2>Contactanos</h2>
-     <p>Mejora la eficiencia energética de tu hogar y reduce tu factura de la luz</p>
-   </div>
     </div>
-    
- 
   );
 }
-
